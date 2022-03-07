ce9d6756f489cc7f6b85fc07ccdf3f0bbefb1755
Adam Kuśmirek <adam.kusmirek@conpeek.com>
2022-03-07 14:55:28 +0100

    progress

--- actions/actions.py
+++ actions/actions.py
@@ -44,6 +44,17 @@ class ActionSessionStart(Action):
         events.append(SlotSet("validate_counter", 0))
         return events
 
+class ActionTriggerResponseSelector(Action):
+
+    def name(self) -> Text:
+        return "action_trigger_response_selector"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any],) -> List[EventType]:
+        retrieval_intent = tracker.get_slot("retrieval_intent")
+        if retrieval_intent:
+            dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+        return [SlotSet("retrieval_intent", None)]
+
 class ActionAsrLowConfidence(Action):
 
     def name(self) -> Text:
